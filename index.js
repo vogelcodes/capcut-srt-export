bcbb9db94e68f554d8a8b5495e2ff9c468de5f89
Daniel Vogel <danielcvt@gmail.com>
2022-12-08 21:59:32 -0300

    search for subtitle info in more tracks, tries different regex patterns

--- index.js
+++ index.js
@@ -20,20 +20,29 @@ const data = extractData();
 const { materials, tracks } = data;
 
 //Habilitar apenas uma dessas linhas removendo o // da frente:
-//const re = /\[(.*?)\]/ //regex que extrai o content entre []'s, alterado na ultima versao do capcut
-const re = /\<size.*?\>(.*?)\<\/size\>/ //regex atual (v1.2.0) que localiza o content entre <size*></size>
+//let re = /\[((.|\n)*?)\]/ //regex que extrai o content entre []'s, alterado na ultima versao do capcut
+let re = /\<size.*?\>((.|\n)*?)\<\/size\>/ //regex atual (v1.2.0) que localiza o content entre <size*></size>
 
-
-const subTiming = tracks[1].segments
+let subTrackNumber = 1
+let subTiming = tracks[subTrackNumber].segments
 var subtitlesInfo = materials.texts.map(i=>{
+
+  if (re.exec(i.content)[1].substring(0,1) === '<') {
+    re = /\[((.|\n)*?)\]/
+  }
   return {
     content: re.exec(i.content)[1],
     id: i.id
   }
 })
+console.log(subtitlesInfo)
 subtitlesInfo = subtitlesInfo.map((s,i) => {
-  const segment = subTiming.find(i => i.material_id === s.id)
-
+  let segment = subTiming.find(i => i.material_id === s.id)
+  while (!segment) {
+    subTrackNumber++
+    subTiming= tracks[subTrackNumber].segments
+    segment = subTiming.find(i => i.material_id === s.id)
+  }
   s.start = segment.target_timerange.start
   s.end = s.start + segment.target_timerange.duration
   s.srtStart = msToSrt(s.start)
