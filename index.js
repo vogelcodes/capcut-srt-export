c107d4ab59a765bf9eb8c999636b46659d4ce855
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2024-01-12 12:24:47 +0000

    CCv.3.2.0 update

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-const fs = require('fs');
+const fs = require("fs");
 
 //this extracts data from a json file
 function extractData(filename) {
   const data = fs.readFileSync(filename);
   return JSON.parse(data);
 }
-function msToSrt(timeInMs){
-  const convertMs= Math.floor(timeInMs / 1000)
-  
-  const ms = convertMs%1000
-  const totalSeconds = (convertMs-ms)/(1000)
-  const seconds = (totalSeconds)%(60)
-  const totalMinutes = (totalSeconds-seconds)/60
-  const minutes = totalMinutes%60
-  const hour = (totalMinutes-minutes)/60
-  return `${hour < 10 ? '0'+hour:hour}:${minutes<10 ? '0'+minutes:minutes}:${seconds<10? '0'+seconds:seconds},${ms}`
+function msToSrt(timeInMs) {
+  const convertMs = Math.floor(timeInMs / 1000);
+
+  const ms = convertMs % 1000;
+  const totalSeconds = (convertMs - ms) / 1000;
+  const seconds = totalSeconds % 60;
+  const totalMinutes = (totalSeconds - seconds) / 60;
+  const minutes = totalMinutes % 60;
+  const hour = (totalMinutes - minutes) / 60;
+  return `${hour < 10 ? "0" + hour : hour}:${
+    minutes < 10 ? "0" + minutes : minutes
+  }:${seconds < 10 ? "0" + seconds : seconds},${ms}`;
 }
 
-var draftFileName = ''
-let os = process.env.os
+var draftFileName = "draft_content.json";
+let os = process.env.os;
 switch (os) {
-  case 'Windows_NT':
-    draftFileName = 'draft_content.json'
+  case "Windows_NT":
+    draftFileName = "draft_content.json";
     break;
-  case 'Darwin':
-    draftFileName = 'draft_info.json'
+  case "Darwin":
+    draftFileName = "draft_info.json";
     break;
-  case 'Linux':
-    draftFileName = 'draft_info.json'
+  case "Linux":
+    draftFileName = "draft_info.json";
 }
-
+console.log("file: " + draftFileName);
 const data = extractData(draftFileName);
 const { materials, tracks } = data;
 
 // let regexOptions = [ /\<size.*?\>((.|\n)*?)\<\/size.*?\>/, /\[((.|\n)*?)\]/ ]
 
-let subTrackNumber = 1
-let subTiming = tracks[subTrackNumber].segments
-var subtitlesInfo = materials.texts.map(i=>{
+let subTrackNumber = 1;
+let subTiming = tracks[subTrackNumber].segments;
+var subtitlesInfo = materials.texts.map((i) => {
   // var matches = regexOptions.map(r => i.content.match(r))
-  let content = i.content.replace(/\<.*?\>/g, '').replace(/\<\/.*?\>/g, '').replace(/\[|\]/g, '')
-
+  let content = i.content
+    .replace(/\<.*?\>/g, "")
+    .replace(/\<\/.*?\>/g, "")
+    .replace(/\[|\]/g, "");
+  try {
+    let contentV3 = JSON.parse(i.content);
+    if (contentV3) {
+      content = contentV3.text;
+    }
+  } catch (error) {}
   // console.log(matches)
   // var content = matches.reduce((acc, m) => {
   //   if (m){
@@ -58,51 +68,49 @@ var subtitlesInfo = materials.texts.map(i=>{
   //   , '')
   return {
     content,
-    id: i.id
-  }
-})
+    id: i.id,
+  };
+});
 // console.log(subtitlesInfo)
-subtitlesInfo = subtitlesInfo.map((s,i) => {
-  let segment = subTiming.find(i => i.material_id === s.id)
+subtitlesInfo = subtitlesInfo.map((s, i) => {
+  let segment = subTiming.find((i) => i.material_id === s.id);
   while (!segment) {
-    subTrackNumber++
-    subTiming= tracks[subTrackNumber].segments
-    segment = subTiming.find(i => i.material_id === s.id)
+    subTrackNumber++;
+    subTiming = tracks[subTrackNumber].segments;
+    segment = subTiming.find((i) => i.material_id === s.id);
   }
-  s.start = segment.target_timerange.start
-  s.end = s.start + segment.target_timerange.duration
-  s.srtStart = msToSrt(s.start)
-  s.srtEnd = msToSrt(s.end)
-  s.subNumber = i+1
-  s.srtTiming = s.srtStart+' --> '+s.srtEnd
+  s.start = segment.target_timerange.start;
+  s.end = s.start + segment.target_timerange.duration;
+  s.srtStart = msToSrt(s.start);
+  s.srtEnd = msToSrt(s.end);
+  s.subNumber = i + 1;
+  s.srtTiming = s.srtStart + " --> " + s.srtEnd;
 
-  return s
-
-
-})
-
+  return s;
+});
 
 // console.log(subtitlesInfo)
-const srtOut = subtitlesInfo.reduce((srt,i)=>{
-  const subtitle = `${i.subNumber}\n${i.srtTiming}\n${i.content}\n\n`
-  return srt+subtitle
-},'')
-const copyOut = subtitlesInfo.reduce((srt,i)=>{
-  const subtitle = `${i.content}\n`
-  return srt+subtitle
-},'')
+const srtOut = subtitlesInfo.reduce((srt, i) => {
+  const subtitle = `${i.subNumber}\n${i.srtTiming}\n${i.content}\n\n`;
+  return srt + subtitle;
+}, "");
+const copyOut = subtitlesInfo.reduce((srt, i) => {
+  const subtitle = `${i.content}\n`;
+  return srt + subtitle;
+}, "");
 //this function writes the string to a file
 function writeToFile(data, filename) {
-  console.log('Saving subtitles to file...')
+  console.log("Saving subtitles to file...");
   fs.writeFileSync(filename, data);
-  console.log('Done!')
+  console.log("Done!");
   // fs.writeFileSync('subtitles.json', JSON.stringify(subtitlesInfo));
 }
-if (process.argv[2] === '--txt') {
-  
-  console.log('Copy extraction is a courtesy of @dellucanil')
-  writeToFile(copyOut, 'copy.txt');
+if (process.argv[2] === "--txt") {
+  console.log("Copy extraction is a courtesy of @dellucanil");
+  writeToFile(copyOut, "copy.txt");
 } else {
-  writeToFile(srtOut, 'subtitles.srt');
-  console.log('Run "node index.js --txt" to get a copy version of the subtitles, courtesy of @dellucanil')
+  writeToFile(srtOut, "subtitles.srt");
+  console.log(
+    'Run "node index.js --txt" to get a copy version of the subtitles, courtesy of @dellucanil'
+  );
 }
